5e3f1760515d7f119c4c0c50745ad6c2127fb5af
ja <ja@zdb.es>
2024-09-17 21:25:07 +0200

    v13

--- frontend/src/components/Login.svelte
+++ frontend/src/components/Login.svelte
@@ -4,7 +4,7 @@
     export let isLogin = false;
 
     async function login() {
-        const response = await fetch('http://localhost:5000/login', {
+        const response = await fetch('http://0.0.0.0:8080/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
